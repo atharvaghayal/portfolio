10285fb024d79ae90a6a715ccb8b878d936cb653
atharvaghayal <atharva160504@gmail.com>
2025-12-18 11:56:48 +0530

    Updated Deloitte card

--- my-site/src/components/WorkEducation/WorkEducation.js
+++ my-site/src/components/WorkEducation/WorkEducation.js
@@ -21,7 +21,7 @@ const workData = [
   },
   {
     id: 2,
-    title: "Deloitte Data analytics virtual intership program",
+    title: "Deloitte Data analytics virtual Internship Program",
     role: "Certification",
     date: "July 2025",
     background: 'linear-gradient(to right, #009639, #00A9E0, #003B5C, #66B032)',
