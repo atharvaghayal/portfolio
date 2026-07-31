e5e760f01f161fa115ba95b1ebe6be16982cdd43
atharvaghayal <atharva160504@gmail.com>
2026-07-31 21:46:52 +0530

    Updated portfolio

--- blogs-site/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders blogs page navigation', () => {
-  render(<App />);
-  expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: /projects/i })).toBeInTheDocument();
-});
--- projects-site/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders projects page navigation and heading', () => {
-  render(<App />);
-  expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: /blogs/i })).toBeInTheDocument();
-  expect(screen.getByText(/want to be part of my projects/i)).toBeInTheDocument();
-});
